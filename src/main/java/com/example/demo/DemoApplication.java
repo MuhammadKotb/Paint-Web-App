ca4e8bd0774405e8ce68ee6574706f8956d80f5d
es-yousef.zaki2019@alexu.edu.eg <nyngob-jYnbab-8tichy>
2021-12-06 03:32:13 +0200

    Joe Begins

--- src/main/java/com/example/demo/DemoApplication.java
+++ src/main/java/com/example/demo/DemoApplication.java
@@ -5,6 +5,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
+
 @RestController
 public class DemoApplication {
 
