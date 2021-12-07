216d7805e8259db35b045b234097c2a69b3b8235
MuhammadElkotb <Muhammad175ib59@gmail.com>
2021-12-07 15:09:51 +0200

    added spring boot project folder

---
+++ Pain-Web_App/src/main/java/PainWeb_App/PainWeb_App/PainWebAppApplication.java
@@ -0,0 +1,13 @@
+package PainWeb_App.PainWeb_App;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PainWebAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PainWebAppApplication.class, args);
+	}
+
+}
---
+++ Pain-Web_App/src/test/java/PainWeb_App/PainWeb_App/PainWebAppApplicationTests.java
@@ -0,0 +1,13 @@
+package PainWeb_App.PainWeb_App;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PainWebAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
